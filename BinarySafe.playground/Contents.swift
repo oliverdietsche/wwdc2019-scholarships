c9c735f993d8fd879d037679a9e811e0628dd61a
Oliver Dietsche <oliver.dietsche@namics.com>
2019-03-24 10:44:21 +0100

    Adds text to the Contents.swift

--- BinarySafe.playground/Contents.swift
+++ BinarySafe.playground/Contents.swift
@@ -1,5 +1,15 @@
 import PlaygroundSupport
 import SpriteKit
 
+/*:
+ # Welcome to the Binary-Safe!
+ 
+ * The safe you have to solve consists of different layers and sections containing binary values.
+ * Rotate the layers until the binary code, read from the inside to the outside, results in the decimal shown on the outside of the safe.
+ * You can check the current value of each section by pressing on the decimal and increase the difficulty by clicking on the home button to adjust the amount of layers and pieces.
+ * Choose help if you need further explanation.
+ * Enjoy!
+ */
+
 let gameViewController = GameViewController()
 PlaygroundSupport.PlaygroundPage.current.liveView = gameViewController
